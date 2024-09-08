644f6c5b7c79420094625f6a6796d89e5035f92d
Paul Sebastian Okoth <okothpaul04@gmail.com>
2024-09-08 16:27:16 +0300

    Update Frame2D.h
    Updated comments section

--- src/Frame2D.h
+++ src/Frame2D.h
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Frame2D.h
- * Author: paul
+ * Author: Paul Sebastian Okoth
  *
  * Created on 29 August 2019, 10:57
  */
